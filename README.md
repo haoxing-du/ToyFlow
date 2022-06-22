c3a06c2a6d524e97737542ec47eca3622c4739d5
ViniciusMikuni <vinicius.mikuni@gmail.com>
2022-06-21 22:43:03 -0700

    Add parallel example

--- README.md
+++ README.md
@@ -33,3 +33,41 @@ Questions:
 * Imagine you started with a random point drawn from the 2-gaussian distribution. How would you determine the conditional value this point belongs to? (Hint: In the toy example, z can only assume 2 values z1, and z2, determine the probability of p(x|z1) and p(x|z2))
 * Change the script such that now the conditional distribution is continuous and describes the mean of a gaussian distribution in 2D with std=1. Train the flow with this new condition and sample
 * Now, imagine again that you have a data point x and want to determine the most likely value for z as p(z|x). Implement a function that maximizes lop(p(z|x)) and determine the z value. Test the algorithm by drawing different values for z and see if you get the correct answer back.
+
+# Part 3
+
+Now, we are going to run the scripts in parallel using the Perlmutter supercomputer! We emply data parallelization using the [Horovod](https://github.com/horovod/horovod) library to split the data used during training between multiple GPUs.
+
+To do so we are going to use the script ```toy_paralle.py``` that implements a couple of additional features besides the parallel training. Check it out and compare with the ```toy_conditional.py``` scripts to see the differences.
+
+A new feature is the implementation of convolutional neural networks as the backbone of the FFJORD implementation. Try out different examples by running the script and changing the parameter ```model_name``` to the available implementations: ```moon, mnist, calorimeter``` and running.
+
+```bash
+python train_paralle.py --model_name moon
+```
+
+All implementaions should run out of the box and give you similar distributions as the ones we looked before. For the calorimeter implementation however there are not yet any distributions defined, which observables are useful to compare the performance of the flow model?
+
+
+Up to now we always used a single GPU during training. Let's scale this up and test how the different implementations behave as we try to use **16** GPUs at a time!
+
+First, hop in to the Perlmutter system through an ssh connection. Once connected, move to the folder you've been using to run your experiments and request 4 iteractive nodes:
+
+```bash
+salloc -C gpu -q interactive  -t 30 -n 16 --ntasks-per-node=4  --gpus-per-task=1 -A [m3929] --gpu-bind=none
+```
+
+Change the -A flag accordingly based on the project number that you are assigned to.
+
+After getting the allocation, you should be almost ready to go. Load any modules you've been using so farand run the script with the srun command:
+
+```bash
+module load tensorflow/2.6.0
+srun python train_paralle.py --model_name moon
+```
+
+This should be enough to spawn the multiple processes. What is the time difference for each epoch between the multi-GPU implementation and the single GPU implementation?
+
+The last task is to investigate the calorimeter data in more detail. The preprocessing adopted mimics the one used in the [CaloFlow paper](https://arxiv.org/abs/2106.05285). As is, we use the fully-connected model as the backbone implementation for the model. However, we do know that the calorimeter can be represented as a set of 3 images, each representing a layer of the calorimeter, with dimensions: 3x96 (in layer 0), 12x12 (in layer 1), and 12x6 (in layer 3). Since the images hold geometric information of the shower shapes, we expect that the densoty estimation based on convolutions to be better than a simple fully connected model (you can see for yourself, verify the loss value on the MNIST dataset when using a fully connected model and when using the convolutional layers).
+
+How would you implement a convolutional model for this dataset?
--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -5,7 +5,19 @@ import numpy as np
 import tensorflow as tf
 
 
-def DataLoader(file_name,nevts):
+def split_data(data,nevts,frac=0.8):
+    data = data.shuffle(nevts)
+    train_data = data.take(int(frac*nevts)).repeat()
+    test_data = data.skip(int(frac*nevts)).repeat()
+    return train_data,test_data
+
+def LoadJson(file_name):
+    import json,yaml
+    JSONPATH = os.path.join(file_name)
+    return yaml.safe_load(open(JSONPATH))
+
+
+def DataLoader(file_name,nevts=-1):
     '''
     Inputs:
     - name of the file to load
@@ -15,11 +27,16 @@ def DataLoader(file_name,nevts):
     - Energy deposition in each layer (nevts,3)
     - Normalized energy deposition per voxel (nevts,504)
     '''
+    import horovod.tensorflow.keras as hvd
+    hvd.init()
+    
     with h5.File(file_name,"r") as h5f:
-        e = h5f['energy'][:int(nevts)].astype(np.float32)
-        layer0= h5f['layer_0'][:int(nevts)].astype(np.float32)
-        layer1= h5f['layer_1'][:int(nevts)].astype(np.float32)
-        layer2= h5f['layer_2'][:int(nevts)].astype(np.float32)
+        if nevts <0:
+            nevts = len(h5f['energy'])
+        e = h5f['energy'][hvd.rank():nevts:hvd.size()].astype(np.float32)
+        layer0= h5f['layer_0'][hvd.rank():nevts:hvd.size()].astype(np.float32)/1000.0
+        layer1= h5f['layer_1'][hvd.rank():nevts:hvd.size()].astype(np.float32)/1000.0
+        layer2= h5f['layer_2'][hvd.rank():nevts:hvd.size()].astype(np.float32)/1000.0
 
     def preprocessing(data):
         ''' 
@@ -29,21 +46,100 @@ def DataLoader(file_name,nevts):
         x = data.shape[1]
         y = data.shape[2]
         data_flat = np.reshape(data,[-1,x*y])
-        #uncomment below to add noise like caloflows does
-        #data_flat +=np.random.uniform(0,1e-3,size=data_flat.shape)
+        #add noise like caloflow does
+        data_flat +=np.random.uniform(0,1e-6,size=data_flat.shape)
         energy_layer = np.sum(data_flat,-1).reshape(-1,1)
         #Some particle showers have no energy deposition at the last layer
         data_flat = np.ma.divide(data_flat,energy_layer).filled(0)
-        return energy_layer,data_flat
+
+        #Log transform from caloflow paper
+        alpha = 1e-6
+        x = alpha + (1 - 2*alpha)*data_flat
+        shower = np.ma.log10(x/(1-x)).filled(0)    
+
+        
+        return energy_layer,shower
 
 
     flat_energy , flat_shower = preprocessing(np.nan_to_num(layer0))    
     for il, layer in enumerate([layer1,layer2]):
         energy ,shower = preprocessing(np.nan_to_num(layer))
         flat_energy = np.concatenate((flat_energy,energy),-1)
-        flat_shower = np.concatenate((flat_shower,shower),-1)
+        flat_shower = np.concatenate((flat_shower,shower),-1)        
         
-    return e,flat_energy,flat_shower
+    return np.log10(e),np.ma.log10(flat_energy/e),flat_shower
+
+
+def Calo_prep(file_path,nevts=-1):
+    energy, energy_layer, energy_voxel = DataLoader(file_path,nevts)
+    # print(energy.shape,energy_layer.shape, energy_voxel.shape)
+    #> (99999, 1) (99999, 3) (99999, 504)
+
+    #Let's try to learn the normalized energy depositions (energy_voxel) and the per layer normalization (energy_layer) simultaneously by concatenating the 2 datasets.
+    
+    tf_data = tf.data.Dataset.from_tensor_slices(np.concatenate([energy_voxel,energy_layer],-1))
+    tf_cond = tf.data.Dataset.from_tensor_slices(energy)
+    samples =tf.data.Dataset.zip((tf_data, tf_cond))
+    nevts=energy.shape[0]
+    frac = 0.8 #Fraction of events used for training
+    train,test = split_data(samples,nevts,frac)
+    return int(frac*nevts), int((1-frac)*nevts), train,test
+
+def MNIST_prep(X,y,nevts=-1):
+    '''
+    Splits the MNIST dataset into equally sized chunks based on 
+    the number of simultaneous processes to run
+        
+    Inputs: Data and label used for conditioning
+    Outputs: Merged dataset used for training and total number of 
+    training events in the split sample
+        '''
+    import horovod.tensorflow.keras as hvd
+    hvd.init()
+    if nevts<0:
+        nevts = y.shape[0]
+
+    y = y.reshape((-1,1))[hvd.rank():nevts:hvd.size()] #Split dataset equally between GPUs
+    y = y.astype('float32')/10.0
+    
+    X = X.reshape(-1,28,28,1)[hvd.rank():nevts:hvd.size()]
+    X = X.astype('float32')
+    X+=np.random.uniform(0,1,X.shape)
+    X /= 256.0
+    
+    tf_data = tf.data.Dataset.from_tensor_slices(X)
+    tf_cond = tf.data.Dataset.from_tensor_slices(y)
+    samples =tf.data.Dataset.zip((tf_data, tf_cond)).shuffle(X.shape[0]).repeat()
+    return y.shape[0],samples
+
+def Moon_prep(X,nevts=-1):
+    '''
+    Splits the Double moon dataset into equally sized chunks based on 
+    the number of simultaneous processes to run
+        
+    Inputs: Data 
+    Outputs: Merged dataset with mock conditional labels to match 
+    the shape expected for training and number of training events in the split sample
+    '''
+    import horovod.tensorflow.keras as hvd
+    if nevts<0:
+        nevts = X.shape[0]
+
+    hvd.init()
+    y = np.ones(X.shape[0],dtype=np.float32)
+    y = y.reshape((-1,1))[hvd.rank():nevts:hvd.size()] #Split dataset equally between GPUs
+    
+        
+    X = X[hvd.rank():nevts:hvd.size()]
+
+    tf_data = tf.data.Dataset.from_tensor_slices(X)
+    tf_cond = tf.data.Dataset.from_tensor_slices(y)
+    samples =tf.data.Dataset.zip((tf_data, tf_cond)).shuffle(X.shape[0]).repeat()
+    return y.shape[0],samples
+
+    
+
+
 
 
 if __name__ == "__main__":
